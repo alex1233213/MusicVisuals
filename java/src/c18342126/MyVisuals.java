b8385e0086950f74c8adebb4b0ef0428a4ac5900
alex1233213 <alexanderbulgari@gmail.com>
2020-03-27 21:50:49 +0000

    commit

--- java/src/c18342126/MyVisuals.java
+++ java/src/c18342126/MyVisuals.java
@@ -9,6 +9,13 @@ public class MyVisuals extends Visual {
     }
 
     public void draw() {
+        startMinim();
+                
+        // Call loadAudio to load an audio file to process 
+        loadAudio("Illusionize.mp3");   
 
+        //create objects
+        // wf = new WaveForm(this);
+        // abv = new AudioBandsVisual(this);
     }
 }
