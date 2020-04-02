d937559ea643d47acba129ee63db4461d869fa0b
alex1233213 <alexanderbulgari@gmail.com>
2020-04-02 14:48:17 +0100

    commit

--- java/src/c18342126/Dots.java
+++ java/src/c18342126/Dots.java
@@ -4,30 +4,42 @@ import processing.core.*;
 
 public class Dots {
     My_Visual mv;
-    float radius = 5f;
+    float diam;
     PVector location;
     PVector velocity;
+
 
     
 
     public Dots(My_Visual mv) {
         this.mv = mv; 
-        location = new PVector( radius, mv.height / 2);
-        velocity = new PVector(2, -2);
+        this.diam = mv.random(20, 30);
+        velocity = new PVector(2, 0);
+        location = new PVector(mv.random(mv.width), mv.random(mv.height));
     }
 
     public void moveDots() {
         location.add(velocity);
+
+
+        //check 
+        if(location.x > mv.random(mv.width * 0.8f, mv.width * 0.9f)) {
+            
+
+            if(this.diam == 0) {
+                this.diam = 0;
+            } else { 
+                this.diam -= 5;
+            }
+        }
     }
         
     
 
     public void render() {
         mv.noStroke();
-        mv.fill(245);
-        
-        mv.ellipse(location.x, location.y, radius * 2, radius * 2);
-        
+        mv.fill(255, 200);
+        mv.ellipse(location.x, location.y, diam, diam);
     }
 }
 
--- java/src/c18342126/My_Visual.java
+++ java/src/c18342126/My_Visual.java
@@ -1,10 +1,11 @@
 package c18342126;
 
+import java.util.ArrayList;
+
 import ie.tudublin.*;
 
 public class My_Visual extends Visual {
-    Dots dots;
-
+    ArrayList<Dots> dots = new ArrayList <Dots> ();
 
     public void settings() {
         size(1200, 800);
@@ -14,8 +15,10 @@ public class My_Visual extends Visual {
         setFrameSize(512);
         startMinim();
         loadAudio("Plakala.mp3"); 
-
-        dots = new Dots(this);
+        
+        
+        
+        
         
     }
 
@@ -34,7 +37,7 @@ public class My_Visual extends Visual {
 
     public void draw() {
         background(196, 0, 240);
-        
+
         try
         {
             // Call this if you want to use FFT data
@@ -44,10 +47,35 @@ public class My_Visual extends Visual {
         {
             e.printStackTrace();
         }
+               
+        for(int i = 0; i < 20; i++) {
+            Dots dot = new Dots(this);
+            dots.add(dot);
+
+
+            if(dots.size() > 300) {
+
+                for(int j = 0; j < dots.size() - 1; j++) {
+                    dots.remove(j);
+                }
                 
+            }
+        }
         
-        dots.render();
-        dots.moveDots();
+    
+        
+        
+
+     
+
+        
+        for(Dots d : dots) {
+            d.render();
+            d.moveDots();
+        }
+
+        
+        
 
     }
 
